cd826ae92f872557df80b4e4168029531aa99522
HoRizon <jiatenghuipl@gmail.com>
2021-12-07 12:55:03 +0800

    1.0

--- main.py
+++ main.py
@@ -19,11 +19,19 @@ def schema_update_collation(collation, schema_id, conn, cursor):
     cursor.execute(sql)
     conn.commit()
 
+
 def get_schema(cursor):
     sql = "select * from `schema`"
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
+
+
+def create_schema(db_name, charset, collation, conn, cursor):
+    sql = "create database {} default character set {} default collate {}".format(db_name, charset,
+                                                                                  collation)
+    cursor.execute(sql)
+    conn.commit()
 
 
 layout = [[sg.Button('Function1'), sg.Button('Function2'), sg.Button('Function3')]]
@@ -37,7 +45,7 @@ while True:
     # Output a message to the window
     if event == 'Function1':
         layout1 = [[sg.Input(key='win1_input')],
-                   [sg.Button('修改字符集'), sg.Button('修改排序方式'), sg.Button('查看元数据')],
+                   [sg.Button('修改字符集'), sg.Button('修改排序方式'), sg.Button('查看元数据'), sg.Button('新建数据库')],
                    [sg.Output(key='result', size=(200, 400))]]
         window1 = sg.Window('Window1', layout1, size=(400, 400))
         while True:
@@ -66,6 +74,21 @@ while True:
                 print("schema_id      schema_name      character_set      collation")
                 for i in output:
                     print("{:^20}{:^20}{:^20}{:^20}".format(i[0], i[1], i[2], i[3]))
+            if ev1 == '新建数据库':
+                window1['result'].update('')
+                if val1['win1_input']:
+                    win1_input = val1['win1_input']
+                    db_name, charset, collation = win1_input.split(' ')
+                    print(db_name, charset, collation)
+                    create_schema(db_name, charset, collation, conn, cursor)
+                    print("新建数据库成功")
+                    cursor.execute("select max(schema_id) from `schema`")
+                    result = cursor.fetchone()
+                    schema_id = str(int(result[0]) + 1)
+                    cursor.execute(
+                        "insert into `schema` values ('{}','{}','{}','{}')".format(schema_id, db_name, charset,
+                                                                                   collation))
+                    conn.commit()
 
     if event == 'Function2':
         layout2 = [[sg.Input(key='win1_input')],
@@ -78,3 +101,4 @@ while True:
                 break
 # Finish up by removing from the screen
 window.close()
+conn.close()
